dbab31907de391bc51bd5aad10febb2171a4729f
ranjan <ranjan.m@kalvium.community>
2024-01-11 10:40:33 +0530

    Done

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -3,11 +3,47 @@ import "./Carousel.css";
 import { images } from "../data/CarouselData";
 
 // you can research - how to use material ui
-import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos'
+import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
 import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 
 // complete the function below:
 function Carousel() {
+  let [currIndex, setCurrIndex] = useState(0);
+
+  function moveBackward() {
+    if (currIndex <= 0) {
+      setCurrIndex(images.length - 1);
+    } else {
+      setCurrIndex(currIndex - 1);
+    }
+  }
+
+  function moveForward() {
+    if (currIndex == images.length - 1) {
+      setCurrIndex(0);
+    } else {
+      setCurrIndex(currIndex + 1);
+    }
+  }
+
+  return (
+    <>
+      <div id="main">
+        <button className="btn" onClick={moveBackward}>
+          <ArrowBackIosIcon />
+        </button>
+        <div id="image">
+          <img src={images[currIndex].img} alt="" />
+          <h1>{images[currIndex].title}</h1>
+          <p>{images[currIndex].subtitle}</p>
+        </div>
+
+        <button className="btn" onClick={moveForward}>
+          <ArrowForwardIosIcon />
+        </button>
+      </div>
+    </>
+  );
 }
 
 export default Carousel;
